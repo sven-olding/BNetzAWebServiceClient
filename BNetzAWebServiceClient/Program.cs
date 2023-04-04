42937b5bdb28f447bbf9221b2078e28a078bff26
Sven Olding <solding@gedys-intraware.de>
2023-04-04 12:54:07 +0200

    update .NET version and service references

--- BNetzAWebServiceClient/Program.cs
+++ BNetzAWebServiceClient/Program.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Net;
-
+﻿
 using log4net;
 using log4net.Config;
-using Bsc;
+using System;
+using System.IO;
+using System.Net;
 using System.Net.Security;
 
 namespace BNetzAWebServiceClient
